ea81f6c0fab4ea836874ed0e4b5bac04ee14892c
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-06-12 01:45:14 -0700

    binary trees

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -18,5 +18,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	return (binary_tree_is_full(tree->left) && (binary_tree_is_full(tree->right)));
+	return (binary_tree_is_full(tree->left) &&
+			(binary_tree_is_full(tree->right)));
 }
